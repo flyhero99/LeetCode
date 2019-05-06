2fae1deeaa6b11c67f8ed0d225a65eb56a1aa86f
flyhero <921285003@qq.com>
2019-05-06 16:25:31 +0800

    Create 27. Remove Element.cpp

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, j = nums.size();
+        while(i < j) {
+            if(nums[i] == val) nums[i] = nums[--j];
+            else i++;
+        }
+        return j;
+    }
+};
